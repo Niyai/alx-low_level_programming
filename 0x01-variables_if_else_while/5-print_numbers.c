233c18eaea19a5822a80d3811fba8977d14bf105
Niyi <akinwaleadeniyi2@gmail.com>
2023-06-21 16:02:28 -0700

    Loope fiasco

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints integers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+	}
+	putchar('\n');
+	return (0);
+}
