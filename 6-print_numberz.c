8ce61797f63456b67889d5daa69e0876f303f5dc
Niyi <akinwaleadeniyi2@gmail.com>
2023-06-21 15:50:52 -0700

    Loope fiasco

---
+++ 6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9 without char or printf functions.
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
